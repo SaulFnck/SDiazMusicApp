d8a318a89fa8f8300386d164fb8afebb5590844b
SaulFnck <sauld5354@gmail.com>
2026-05-07 23:02:16 -0600

    Change listname to Album

--- app/src/main/java/com/example/sdiazmusicapp/Services/albumService.kt
+++ app/src/main/java/com/example/sdiazmusicapp/Services/albumService.kt
@@ -4,12 +4,12 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface songsService {
-/*
-    @GET("products")
+
+    @GET("songs")
     suspend fun getAllProducts() : List<Product>
 
     //Path( /1 ), queryString ( ?name=Juan ) y Body ( {"name":"Juan"} )
     @GET("products/{asasas}")
     suspend fun getProductById(@Path("asasas") id: Int) : Product
-    */
+
 }
