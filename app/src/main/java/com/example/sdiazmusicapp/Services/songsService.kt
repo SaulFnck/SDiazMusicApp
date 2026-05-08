6ebb91182dabd99d7f21e280d46f3de3d9018204
SaulFnck <sauld5354@gmail.com>
2026-05-07 22:57:25 -0600

    Add songs from api

--- app/src/main/java/com/example/sdiazmusicapp/Services/songsService.kt
+++ app/src/main/java/com/example/sdiazmusicapp/Services/songsService.kt
@@ -1,4 +1,15 @@
 package com.example.sdiazmusicapp.Services
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface songsService {
+/*
+    @GET("products")
+    suspend fun getAllProducts() : List<Product>
+
+    //Path( /1 ), queryString ( ?name=Juan ) y Body ( {"name":"Juan"} )
+    @GET("products/{asasas}")
+    suspend fun getProductById(@Path("asasas") id: Int) : Product
+    */
 }
