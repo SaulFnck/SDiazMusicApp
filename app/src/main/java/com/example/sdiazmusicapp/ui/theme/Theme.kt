a0072814b76ce990ebefc078ef033e282fb6209a
SaulFnck <sauld5354@gmail.com>
2026-05-07 23:13:32 -0600

    configuration for Theme.kt

--- app/src/main/java/com/example/sdiazmusicapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/sdiazmusicapp/ui/theme/Theme.kt
@@ -14,30 +14,31 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = DarkPurple,
+    surface = DarkPurple,
+    onPrimary = White,
+    onBackground = White,
+    onSurface = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = PrimaryPurple,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = Pink40,
+    background = AppBackground,
+    surface = CardBackground,
+    onPrimary = White,
+    onBackground = DarkPurple,
+    onSurface = DarkPurple,
+    onSecondaryContainer = SecondaryText
 )
 
 @Composable
 fun SDiazMusicAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false, // Set to false to prioritize the custom theme
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
