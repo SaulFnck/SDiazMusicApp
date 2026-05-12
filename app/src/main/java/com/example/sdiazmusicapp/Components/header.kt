4f46b3edda011b7b099b17f18565694a0b51172e
SaulFnck <sauld5354@gmail.com>
2026-05-11 23:01:19 -0600

    Header component

--- app/src/main/java/com/example/sdiazmusicapp/Components/header.kt
+++ app/src/main/java/com/example/sdiazmusicapp/Components/header.kt
@@ -1,2 +1,89 @@
 package com.example.sdiazmusicapp.Components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sdiazmusicapp.ui.theme.SDiazMusicAppTheme
+
+@Composable
+fun Header(){
+
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .fillMaxWidth()
+            .height(160.dp)
+            .background(MaterialTheme.colorScheme.primary)
+
+    ) {
+        //Iconos
+        Row(
+            modifier = Modifier
+                .padding(15.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            //Menu hamburgueza (Icon)
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Menu",
+                tint = MaterialTheme.colorScheme.surface
+            )
+
+            //Menu search
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search",
+                tint = MaterialTheme.colorScheme.surface
+            )
+
+        }
+
+        //Welcome message
+        Text(
+            text = "Good Morning!",
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.SemiBold
+            ),
+            modifier = Modifier.padding(start = 20.dp),
+            color = MaterialTheme.colorScheme.surface.copy(alpha = 0.9f)
+        )
+
+        //User name
+        Text(
+            text = "Saul Diaz",
+            style = MaterialTheme.typography.displayLarge.copy(
+                fontWeight = FontWeight.ExtraBold,
+                fontSize = 32.sp
+            ),
+            modifier = Modifier.padding(start = 20.dp, top = 4.dp),
+            color = MaterialTheme.colorScheme.surface
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HeaderPreview(){
+    SDiazMusicAppTheme {
+        Header()
+    }
+}
